2d585ae9dfa93f55289e7fc05646ccd0e3381767
hadaracobas <34766865+hadaracobas@users.noreply.github.com>
2020-11-24 16:38:48 +0100

    homepage first functions to get and filter data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
+import React, { useEffect, useState } from "react";
 import "./App.scss";
+import axios from "axios";
+
+// import components
+import Home from "./components/Home";
 
 function App() {
+  const [data, setData] = useState(false);
+
+  const getDataFromSheet = async () => {
+    const dataSheet = await axios
+      .get("https://sheet.best/api/sheets/6c613560-926d-4171-8892-5ba0bae57c44")
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // disable to save cost of request during work
+  /*
+  useEffect(() => {
+    getDataFromSheet();
+  }, []);
+*/
   return (
     <div className="app">
-      <h1>this is demo app</h1>
+      <Home data={data} />
     </div>
   );
 }
---
+++ src/components/Home/index.js
@@ -0,0 +1,190 @@
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+
+function Home(props) {
+  const [filledRowsFromData, setFilledRowsFromData] = useState(false);
+  const [lastMonthRows, setLastMonthRows] = useState(false);
+  const [lastWeekRows, setLastWeekRows] = useState(false);
+  const [todayRows, setTodayRows] = useState(false);
+  const [rowsUpToRate6, setRowsUpToRate6] = useState(false);
+
+  console.log(
+    "all filled rows: ",
+    filledRowsFromData,
+    " last month: ",
+    lastMonthRows,
+    " last week: ",
+    lastWeekRows,
+    " today rows: ",
+    todayRows,
+    " up to rate 6: ",
+    rowsUpToRate6
+  );
+
+  // function to filter, get all filled rows  and store in state
+  const getFilledRowsFromData = () => {
+    let storeFilledRows;
+    if (props.data) {
+      storeFilledRows = props.data.filter((row) => {
+        return row.ID !== "";
+      });
+      setFilledRowsFromData(storeFilledRows);
+    }
+  };
+
+  // function return current month full data
+  const getCurrentDate = () => {
+    let d = new Date();
+    let year = d.getFullYear();
+    let month = d.getMonth() + 1;
+    let day = d.getDate();
+    let hour = d.getHours();
+    let minute = d.getMinutes();
+    let second = d.getSeconds();
+    let builtDate =
+      day.toString() + "/" + month.toString() + "/" + year.toString();
+    let builtHour =
+      hour.toString() + ":" + minute.toString() + ":" + second.toString();
+
+    return builtDate;
+  };
+
+  // function return prev month full data
+  const getPrevMonthDate = () => {
+    let d = new Date();
+    let year = d.getFullYear();
+    let month = d.getMonth();
+    let day = d.getDate();
+
+    let prevDate =
+      day.toString() + "/" + month.toString() + "/" + year.toString();
+
+    return prevDate;
+  };
+
+  // function to filter, get all rows from the last month and store in state
+  const getRowsLastMonth = () => {
+    if (filledRowsFromData) {
+      let getRelevantRows = filledRowsFromData.filter((ele) => {
+        let splitedCurrentDate = getCurrentDate().split("/");
+        let curDate = new Date(
+          splitedCurrentDate[2],
+          parseInt(splitedCurrentDate[1]) - 1,
+          splitedCurrentDate[0]
+        );
+
+        let splitedPrevMonthDate = getPrevMonthDate().split("/");
+        let preDate = new Date(
+          splitedPrevMonthDate[2],
+          parseInt(splitedPrevMonthDate[1]) - 1,
+          splitedPrevMonthDate[0]
+        );
+
+        let rowOfSheet = ele.Date.split("/");
+        let rowDate = new Date(
+          rowOfSheet[2],
+          parseInt(rowOfSheet[1]) - 1,
+          rowOfSheet[0]
+        );
+
+        return rowDate >= preDate && rowDate <= curDate;
+      });
+
+      setLastMonthRows(getRelevantRows);
+    }
+  };
+
+  // function to filter, get all rows from the last week and store in state
+  const getRowsLastWeek = () => {
+    if (filledRowsFromData) {
+      let getRelevantRows = filledRowsFromData.filter((ele) => {
+        let splitedCurrentDate = getCurrentDate().split("/");
+        let curDate = new Date(
+          splitedCurrentDate[2],
+          parseInt(splitedCurrentDate[1]) - 1,
+          splitedCurrentDate[0]
+        );
+
+        let splitedPrevWeekDate = getCurrentDate().split("/");
+        let preDate = new Date(
+          splitedPrevWeekDate[2],
+          parseInt(splitedPrevWeekDate[1]) - 1,
+          splitedPrevWeekDate[0] - 7
+        );
+
+        let rowOfSheet = ele.Date.split("/");
+        let rowDate = new Date(
+          rowOfSheet[2],
+          parseInt(rowOfSheet[1]) - 1,
+          rowOfSheet[0]
+        );
+
+        return rowDate >= preDate && rowDate <= curDate;
+      });
+      setLastWeekRows(getRelevantRows);
+    }
+  };
+
+  // function to filter, get all rows from today and store in state
+  const getRowsToday = () => {
+    if (filledRowsFromData) {
+      let getRelevantRows = filledRowsFromData.filter((ele) => {
+        let curDate = getCurrentDate();
+
+        let rowDate = ele.Date;
+
+        return curDate == rowDate;
+      });
+      setTodayRows(getRelevantRows);
+    }
+  };
+
+  // function to filter, get all rows up to rate 6
+  const getRowsUpToRate6 = () => {
+    if (filledRowsFromData) {
+      let getRelevantRows = filledRowsFromData.filter((ele) => {
+        return parseInt(ele.LeadRate) >= 6;
+      });
+      setRowsUpToRate6(getRelevantRows);
+    }
+  };
+
+  // fire function get all filled rows
+  useEffect(() => {
+    getFilledRowsFromData();
+  }, [props.data]);
+
+  // fire function get rows of last month, last week, today and up to rate 6
+  useEffect(() => {
+    getRowsLastMonth();
+    getRowsLastWeek();
+    getRowsToday();
+    getRowsUpToRate6();
+  }, [filledRowsFromData]);
+
+  return (
+    <div className="home">
+      <h1 className="home__title">Dance Studio Leads Sheet Manager - Demo</h1>
+      <div className="home__content">
+        <div className="home__content--numSummeryContainer">
+          <h4>סך הכל לידים בטבלה</h4>
+          <p>{filledRowsFromData.length}</p>
+        </div>
+        <div className="home__content--numSummeryContainer">
+          <h4>סך הכל לידים החודש</h4>
+          <p>{lastMonthRows.length}</p>
+          <h4>סך הכל לידים השבוע</h4>
+          <p>{lastWeekRows.length}</p>
+          <h4>סך הכל לידים היום</h4>
+          <p>{todayRows.length}</p>
+        </div>
+        <div className="home__content--numSummeryContainer">
+          <h4>סך הכל לידים משתלמים מעל דירוג 6</h4>
+          <p>{rowsUpToRate6.length}</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
